eea112d60d8e6386a165f638f84ae8cf1c6164d5
khiemnv <khiemnv@rikkeisoft.com>
2017-05-05 09:41:42 +0700

    :sparkles: [Security with AngularJS]

---
+++ SecurityAndAngularJS/src/main/java/demo/UiApplication.java
@@ -0,0 +1,45 @@
+package demo;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableOAuth2Sso
+public class UiApplication extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	protected OAuth2RestTemplate OAuth2RestTemplate(
+			OAuth2ProtectedResourceDetails resource, OAuth2ClientContext context) {
+		return new OAuth2RestTemplate(resource, context);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiApplication.class, args);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/index.html", "/home.html", "/")
+				.permitAll().anyRequest().authenticated().and()
+				.csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
+
+}
